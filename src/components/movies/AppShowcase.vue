1dc8b8a31f526ef32518048a99bfba7151c4aeec
Angel Aguirre <angelaguirrel@hotmail.com>
2022-09-03 21:31:58 -0500

    Fixed responsive columns when showing movie posters

--- src/components/movies/AppShowcase.vue
+++ src/components/movies/AppShowcase.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container my-0 mx-auto mt-4 mb-6 grid grid-cols-4 gap-y-16">
+    <div class="container my-0 mx-auto mt-4 mb-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-y-16">
         <AppPoster />
         <AppPoster />
         <AppPoster />
